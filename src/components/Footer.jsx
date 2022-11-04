f0c9e33047a53b6c9da2949b6f8151ec9f814c29
Harold Varde <harvardii15@gmail.com>
2022-11-04 21:42:40 +0800

    mod: finish footer + dep

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -44,14 +44,21 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="">
-        <p className="font-main font-light text-center text-[18px] leading-[27px] text-white">
+      <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[2px] border-t-[#696969]">
+        <p className="font-main font-medium text-center text-[18px] leading-[27px] text-white">
           2022 Hoobank. All Rights Reserved.
         </p>
 
         <div className="flex flex-row md:mt-0 mt-6">
           {socialMedia.map((social, index) => (
-            <img key={social.id} src={social.icon} alt={social.id} />
+            <img
+              key={social.id}
+              src={social.icon}
+              alt={social.id}
+              className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+                index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+              }`}
+            />
           ))}
         </div>
       </div>
